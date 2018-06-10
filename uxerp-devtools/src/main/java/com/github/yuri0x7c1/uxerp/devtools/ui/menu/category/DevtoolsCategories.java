305f059c85992c3204cea457809534f2a6db56f7
yuri0x7c1 <yuri0x7c1@gmail.com>
2018-06-10 14:55:53 +0300

    Devtools icon

--- uxerp-devtools/src/main/java/com/github/yuri0x7c1/uxerp/devtools/ui/menu/category/DevtoolsCategories.java
+++ uxerp-devtools/src/main/java/com/github/yuri0x7c1/uxerp/devtools/ui/menu/category/DevtoolsCategories.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Component;
 
 import com.github.yuri0x7c1.uxerp.common.ui.menu.annotation.MenuItem;
 import com.github.yuri0x7c1.uxerp.common.ui.menu.annotation.MenuItems;
+import com.vaadin.icons.VaadinIcons;
 
 @Component
 @MenuItems({
-    @MenuItem(id = DevtoolsCategories.DEVTOOLS, caption = "Devtools", category = true),
+    @MenuItem(id = DevtoolsCategories.DEVTOOLS, caption = "Devtools", icon=VaadinIcons.WRENCH, category = true),
 })
 public class DevtoolsCategories {
 	public static final String DEVTOOLS = "devtools";
