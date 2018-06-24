5ebe4ab9c9a67787263c880bb6b874d34daf392e
yuri0x7c1 <yuri0x7c1@gmail.com>
2018-06-24 22:10:40 +0300

    Use TreeGree component instead Tree

--- uxcrm-content/src/main/java/com/github/yuri0x7c1/uxcrm/content/ui/component/WebSiteTree.java
+++ uxcrm-content/src/main/java/com/github/yuri0x7c1/uxcrm/content/ui/component/WebSiteTree.java
@@ -1,5 +1,7 @@
 package com.github.yuri0x7c1.uxcrm.content.ui.component;
 
+import java.util.Arrays;
+
 import javax.annotation.PostConstruct;
 
 import org.apache.ofbiz.content.content.service.ContentAssocDataResourceViewFromService;
@@ -9,16 +11,19 @@ import org.apache.ofbiz.webapp.website.WebSite;
 import org.apache.ofbiz.webapp.website.service.WebSiteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.spring.i18n.I18N;
+import org.vaadin.viritin.button.MButton;
 
 import com.github.yuri0x7c1.uxcrm.content.data.WebSiteTreeDataProvider;
 import com.github.yuri0x7c1.uxcrm.content.data.WebSiteTreeNode;
+import com.vaadin.icons.VaadinIcons;
 import com.vaadin.spring.annotation.SpringComponent;
 import com.vaadin.spring.annotation.UIScope;
 import com.vaadin.ui.ComboBox;
-import com.vaadin.ui.Tree;
+import com.vaadin.ui.Notification;
+import com.vaadin.ui.TreeGrid;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.themes.ValoTheme;
 
-import edu.emory.mathcs.backport.java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -42,10 +47,19 @@ public class WebSiteTree extends VerticalLayout {
 
 	ComboBox<WebSite> select = new ComboBox<>();
 
-	Tree<WebSiteTreeNode> tree = new Tree<>();
+	TreeGrid<WebSiteTreeNode> tree = new TreeGrid<>();
 
 	@PostConstruct
 	private void init() {
+		tree.addColumn(WebSiteTreeNode::getContentId).setCaption("Id");
+		tree.addColumn(WebSiteTreeNode::getContentName).setCaption("Name");
+		tree.addComponentColumn(node -> {
+		    MButton button = new MButton(VaadinIcons.PLUS,
+		    	event -> Notification.show("Clicked: " + node.toString())
+		    ).withStyleName(ValoTheme.BUTTON_TINY);
+		    return button;
+		});
+
 		select.setWidthUndefined();
 		select.setItemCaptionGenerator(webSite -> webSite.getSiteName() + "[" + webSite.getWebSiteId() + "]");
 
