f08c74b3ffdb50554c69a76074075927f6234d35
yuri0x7c1 <yuri0x7c1@gmail.com>
2018-08-03 14:33:01 +0300

    Package test

--- uxcrm-app/src/main/java/com/github/yuri0x7c1/uxcrm/Application.java
+++ uxcrm-app/src/main/java/com/github/yuri0x7c1/uxcrm/Application.java
@@ -2,12 +2,13 @@ package com.github.yuri0x7c1.uxcrm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @SpringBootApplication(scanBasePackages = {"com.github.yuri0x7c1.uxcrm", "org.apache.ofbiz"})
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
