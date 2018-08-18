d537613bc6e11dc8d2119262c40691c9a3156f22
yuri0x7c1 <yuri0x7c1@gmail.com>
2018-08-18 17:08:18 +0300

    Remove logout button from side bar

--- uxcrm-app/src/main/java/com/github/yuri0x7c1/uxcrm/operation/LogoutOperation.java
+++ uxcrm-app/src/main/java/com/github/yuri0x7c1/uxcrm/operation/LogoutOperation.java
@@ -2,16 +2,9 @@ package com.github.yuri0x7c1.uxcrm.operation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.vaadin.spring.security.VaadinSecurity;
-import org.vaadin.spring.sidebar.annotation.FontAwesomeIcon;
-import org.vaadin.spring.sidebar.annotation.SideBarItem;
-import org.vaadin.spring.sidebar.annotation.VaadinFontIcon;
 
-import com.github.yuri0x7c1.uxcrm.common.ui.menu.category.CommonCategories;
-import com.vaadin.icons.VaadinIcons;
 import com.vaadin.spring.annotation.SpringComponent;
 
-@VaadinFontIcon(VaadinIcons.POWER_OFF)
-@SideBarItem(sectionId = CommonCategories.OPERATIONS, caption = "Logout")
 @SpringComponent
 public class LogoutOperation implements Runnable {
 
